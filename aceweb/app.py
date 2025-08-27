d0a97b1da51849dcf3018ba97f7794ca7873175b
rohitandani <32292221+rohitandani@users.noreply.github.com>
2025-08-27 10:27:33 -0700

    Update app.py

--- aceweb/app.py
+++ aceweb/app.py
@@ -122,4 +122,5 @@ def download(fname):
     return send_from_directory(str(OUT_DIR), fname, as_attachment=False)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
